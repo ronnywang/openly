557bf207866bc62d7f2254b993be23a28236a750
Ronny Wang <ronnywang@gmail.com>
2023-02-17 15:49:23 +0800

    從議案列表抓取網頁內容

---
+++ bill/crawl-entry.php
@@ -0,0 +1,16 @@
+<?php
+
+$fp = fopen($_SERVER['argv'][1], 'r');
+$seq = 0;
+$total = 0;
+while ($obj = json_decode(fgets($fp))) {
+    $id = $obj->id;
+    $total ++;
+    if (!file_exists(__DIR__ . "/bill-html/{$id}.gz")) {
+        $seq ++ ;
+        sleep(1);
+        error_log("{$seq}/{$total} fetching {$id}");
+        $content = file_get_contents("https://ppg.ly.gov.tw/ppg/bills/{$id}/details");
+        file_put_contents(__DIR__ . "/bill-html/{$id}.gz", gzencode($content));
+    }
+}
