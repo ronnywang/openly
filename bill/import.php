f33c048d8740bd5aa4e35323f17863ee184ff125
Ronny Wang <ronnywang@gmail.com>
2023-03-24 18:07:27 +0800

    從上次的地方繼續更新

--- bill/import.php
+++ bill/import.php
@@ -4,8 +4,19 @@ include(__DIR__ . '/../init.inc.php');
 include(__DIR__ . '/Parser.php');
 
 $list = Parser::getListFromWeb();
+$ret = LYLib::dbQuery("/bill/_search?format=json&human", "GET", json_encode([
+    'size' => 0,
+    'aggs' => [
+        'max_mtime' => [ 'max' => [ 'field' => 'mtime']],
+    ],
+]));
+$max_value = json_decode($ret)->aggregations->max_mtime->value;
+
 foreach ($list as $idx => $v) {
     list($filename, $time) = $v;
+    if ($time < $max_value) {
+        continue;
+    }
     list($billNo) = explode('.', $filename);
     error_log($idx . ' ' . $billNo);
     $mtime = filemtime(__DIR__ . "/bill-html/{$billNo}.gz");
